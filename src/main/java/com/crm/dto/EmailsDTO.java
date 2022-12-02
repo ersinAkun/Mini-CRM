c6172505a1ff57cdd198be733f44daac020d6a95
Ersin Akun <ersinakun34@gmail.com>
2022-12-02 12:51:00 +0300

    v1

--- src/main/java/com/crm/dto/EmailsDTO.java
+++ src/main/java/com/crm/dto/EmailsDTO.java
@@ -1,6 +1,10 @@
 package com.crm.dto;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,4 +17,12 @@ import lombok.Setter;
 @Entity
 public class EmailsDTO {
 
+       
+    @NotEmpty(message = "Email cannot be left blank.")
+    @Email
+    @Size(min = 5, max = 50,  message = "Email must be between 5 and 50 characters")
+    @NotNull(message = "Please enter email")  
+    private String email;
 }
+
+
