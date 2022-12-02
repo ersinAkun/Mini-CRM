7cd1f3f0afaf7da5a591ffe88222f7fa07b33b20
Mehmet Emin Çatal <mhmtmnctl20@gmail.com>
2022-12-02 16:08:59 +0300

    emin

--- src/main/java/com/crm/repository/OrderInformationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.crm.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface OrderInformationRepository extends JpaRepository<T, ID>{
-
-}
