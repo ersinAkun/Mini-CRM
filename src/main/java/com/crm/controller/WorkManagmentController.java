ed6d0e269e69980d33c559b457794c52323bbb86
Selim Polat <0mfinci@gmail.com>
2022-12-06 23:04:38 +0300

    slm01

---
+++ src/main/java/com/crm/controller/WorkManagmentController.java
@@ -0,0 +1,24 @@
+package com.crm.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/works")
+public class WorkManagmentController {
+	
+	private WorkManagmentController workManagmentController;
+	
+//************************* CREATE **************************************
+//************************* UPDATE **************************************
+//************************* DELETE **************************************
+//************************* GET *****************************************
+//************************* GET ALL *************************************
+//************************* GET PAGEABLE ********************************
+//************************* getWorksWithReporter ************************
+//************************* getWorksWithStatus **************************
+//************************* getWorksWithPriority ************************
+	
+
+}
