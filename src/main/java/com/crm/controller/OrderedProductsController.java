e5033a9d230614fafaa29899763be773617b60fd
Mehmet Emin Çatal <mhmtmnctl20@gmail.com>
2022-12-07 09:20:37 +0300

    EMİN--ImageFileController--07.12.22

--- src/main/java/com/crm/controller/OrderedProductsController.java
+++ src/main/java/com/crm/controller/OrderedProductsController.java
@@ -13,4 +13,6 @@ public class OrderedProductsController {
 	@Autowired
 	OrderedProductsService orderedProductsService;
 	
+	
+	
 }
--- src/main/java/com/crm/requestDTO/EmailsRequestDTO.java
+++ src/main/java/com/crm/requestDTO/EmailsRequestDTO.java
@@ -1,8 +1,5 @@
 package com.crm.requestDTO;
 
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -14,8 +11,6 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-@Table(name = "tbl_email")
 public class EmailsRequestDTO {
 
     
