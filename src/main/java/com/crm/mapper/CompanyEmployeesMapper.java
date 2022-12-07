2fd9e8194cc89043581e9a96d1f695c8c91a2604
Emrullah Celebi <cumali20182019@gmail.com>
2022-12-07 14:08:15 +0100

    celebi-7.12.22-CompanyEmployeers-creat

--- src/main/java/com/crm/mapper/CompanyEmployeesMapper.java
+++ src/main/java/com/crm/mapper/CompanyEmployeesMapper.java
@@ -1,8 +1,21 @@
 package com.crm.mapper;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
-@Mapper
+
+import com.crm.domain.CompanyEmployees;
+import com.crm.requestDTO.CompanyEmployeesRequestDTO;
+@Mapper(componentModel = "spring")
 public interface CompanyEmployeesMapper{
+	
+	
+	CompanyEmployeesRequestDTO companyEmployeesToCompanyEmployeesRequestDTO(CompanyEmployees companyEmployees);
+	
+	
+	CompanyEmployees companyEmployeesRequestDTOToCompanyEmployees(CompanyEmployeesRequestDTO companyEmployeesRequestDTO);
+	
+	List<CompanyEmployeesRequestDTO> companyEmployeesListToCompanyEmployeesRequestDTOList(List<CompanyEmployees> companyEmployeesList);
 	
 	
 
--- src/main/java/com/crm/service/CompanyEmployeesService.java
+++ src/main/java/com/crm/service/CompanyEmployeesService.java
@@ -1,8 +1,13 @@
 package com.crm.service;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.crm.repository.CompanyEmployeesRepository;
+import com.crm.requestDTO.CompanyEmployeesRequestDTO;
+import com.realestate.exception.ConflictException;
+import com.realestate.exception.message.ErrorMessage;
 
 @Service
 public class CompanyEmployeesService {
@@ -10,5 +15,15 @@ public class CompanyEmployeesService {
 	
 	@Autowired
 	private CompanyEmployeesRepository companyEmployeesRepository;
+
+	public void createCompanyEmployees(CompanyEmployeesRequestDTO companyEmployeesRequestDTO) {
+		if (companyEmployeesRepository.existsByEmail(companyEmployeesRequestDTO.getEmail())) {
+			throw new ConflictException(
+					String.format(ErrorMessage.EMAIL_ALREADY_EXIST_MESSAGE, companyEmployeesRequestDTO.getEmail()));
+		}
+		
+		
+		
+	}
 
 }
