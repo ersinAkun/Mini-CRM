7e4098281667c9046c9277765eff707e8b7488fd
Emrullah Celebi <cumali20182019@gmail.com>
2022-12-08 06:25:53 +0100

    celebi-creat-8.12.22

--- src/main/java/com/crm/repository/CompanyEmployeesRepository.java
+++ src/main/java/com/crm/repository/CompanyEmployeesRepository.java
@@ -1,14 +1,21 @@
 package com.crm.repository;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.crm.domain.CompanyEmployees;
+
 
 @Repository
 public interface CompanyEmployeesRepository extends JpaRepository<CompanyEmployees, Long>   {
 
 	
 	Boolean existsByEmail(String email);
+
+	Object findByEmail(String email);
+	
+	
 	
 	
 }
--- src/main/java/com/crm/service/CompanyEmployeesService.java
+++ src/main/java/com/crm/service/CompanyEmployeesService.java
@@ -1,36 +1,89 @@
 package com.crm.service;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.HashSet;
+import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 import com.crm.domain.CompanyEmployees;
+import com.crm.domain.Role;
+import com.crm.domain.enums.RoleType;
 import com.crm.exception.ConflictException;
 import com.crm.exception.message.ErrorMessage;
 import com.crm.repository.CompanyEmployeesRepository;
 import com.crm.requestDTO.CompanyEmployeesRequestDTO;
+import com.visionrent.domain.User;
+import com.visionrent.exception.ResourceNotFoundException;
+
+
 
 
 @Service
 public class CompanyEmployeesService {
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@Autowired
 	private CompanyEmployeesRepository companyEmployeesRepository;
+	
+	@Autowired
+	private RoleService roleService;
+	
+	
+	@Autowired
+	public CompanyEmployeesService(PasswordEncoder passwordEncoder,
+			CompanyEmployeesRepository companyEmployeesRepository, RoleService roleService) {
+		super();
+		this.passwordEncoder = passwordEncoder;
+		this.companyEmployeesRepository = companyEmployeesRepository;
+		this.roleService = roleService;
+	}
 
 	public void createCompanyEmployees(CompanyEmployeesRequestDTO companyEmployeesRequestDTO) {
 		if (companyEmployeesRepository.existsByEmail(companyEmployeesRequestDTO.getEmail())) {
 			throw new ConflictException(
 					String.format(ErrorMessage.EMAIL_ALREADY_EXIST_MESSAGE, companyEmployeesRequestDTO.getEmail()));
-			
 		}
-			
+		
+		Role role = roleService.findByType(RoleType.ROLE_USER);
+		Set<Role> roles = new HashSet<>();
+		roles.add(role);
+		
+		String encodedPassword = passwordEncoder.encode(companyEmployeesRequestDTO.getPassword());
+		
+		CompanyEmployees companyEmployees = new CompanyEmployees();
+		
+		companyEmployees.setPassword(encodedPassword);
+		companyEmployees.setAddress(companyEmployeesRequestDTO.getAddress());
+		companyEmployees.setCity(companyEmployeesRequestDTO.getCity());
+		companyEmployees.setCountry(companyEmployeesRequestDTO.getCountry());
+		companyEmployees.setEmail(companyEmployeesRequestDTO.getEmail());
+		companyEmployees.setEmployeeDepartment(companyEmployeesRequestDTO.getEmployeeDepartment());
+		companyEmployees.setFirstName(companyEmployeesRequestDTO.getFirstName());
+		companyEmployees.setHasWhatsapp(companyEmployeesRequestDTO.getHasWhatsapp());
+		companyEmployees.setJobTitle(companyEmployeesRequestDTO.getJobTitle());
+		companyEmployees.setLastName(companyEmployeesRequestDTO.getLastName());
+		companyEmployees.setNotes(companyEmployeesRequestDTO.getNotes());
+		companyEmployees.setPhoneNumber(companyEmployeesRequestDTO.getPhoneNumber());
+		companyEmployees.setSpeaks(companyEmployeesRequestDTO.getSpeaks());
+		companyEmployees.setState(companyEmployeesRequestDTO.getState());
+	
+		companyEmployeesRepository.save(companyEmployees);
+		
 	}
 
 		//security için
 	public CompanyEmployees getEmployeeByEmail(String email) {
-		// BURASI EMRULLAH HOCADA.. security için lazım. direkt pojo olarak
-		return null;
+	
+		CompanyEmployees companyEmployees  = companyEmployeesRepository.findByEmail(email).orElseThrow(()->
+		new ResourceNotFoundException(String.format(ErrorMessage.EMPLOYEES_NOT_FOUND_MESSAGE,email))
+		
+				);
+				
+		return companyEmployees ;
 	}
 
 
