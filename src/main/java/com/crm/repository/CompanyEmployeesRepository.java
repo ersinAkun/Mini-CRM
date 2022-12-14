eed77a5079c396c2b293a5f04eb4c66781129bb7
Emrullah Celebi <cumali20182019@gmail.com>
2022-12-14 13:19:43 +0100

    celebi-update-14.12.22

--- src/main/java/com/crm/repository/CompanyEmployeesRepository.java
+++ src/main/java/com/crm/repository/CompanyEmployeesRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.crm.domain.CompanyEmployees;
+import com.crm.domain.enums.Department;
 
 
 @Repository
@@ -32,26 +33,26 @@ public interface CompanyEmployeesRepository extends JpaRepository<CompanyEmploye
 	
 	@Modifying // JpaRepository içinde custom bir query ile DML operasyonları yapılıyor ise  @Modifying konulur
 	   @Query( "UPDATE CompanyEmployees e SET e.firstName=:firstName, e.lastName=:lastName,e.phoneNumber=:phoneNumber,"
-	   		+ "e.email=:email,e.address=:address, e.password=:password, e.city=:city, e.country=:country, "
+	   		+ "e.email=:email,e.address=:address, e.city=:city, e.country=:country, "
 	   		+ "e.employeeDepartment=:employeeDepartment, e.hasWhatsapp=:hasWhatsapp, e.jobTitle=:jobTitle, e.notes=:notes, "
 	   		+ "e.speaks=:speaks, e.state=:state  "
 	   		+ "WHERE e.id=:id" )
 	   void update(@Param("id") Long id,
-			   					 @Param("firstName") String firstName,
-			   					 @Param("lastName") String lastName,
-			   					 @Param("phoneNumber") String phoneNumber,
-			   					 @Param("email") String email,
-			   					 @Param("address") String address,
-			   					@Param("password") String password,
-			   					@Param("city") String city,
-			   					@Param("country") String country,
-			   					@Param("employeeDepartment") String employeeDepartment,
-			   					@Param("hasWhatsapp") Boolean hasWhatsapp,
-			   					@Param("jobTitle") String jobTitle,
-			   					@Param("notes") String notes,
-			   					@Param("speaks") String speaks
-			   					
-			   					);
+			   		@Param("firstName") String firstName,
+			   		@Param("lastName") String lastName,
+			   		@Param("phoneNumber") String phoneNumber,
+			   		@Param("email") String email,
+			   		@Param("address") String address,
+			   		@Param("city") String city,
+			   		@Param("country") String country,
+			   		@Param("employeeDepartment") Department department,
+			   		@Param("hasWhatsapp") Boolean hasWhatsapp,
+			   		@Param("jobTitle") String jobTitle,
+			   		@Param("notes") String notes,
+			   		@Param("speaks") String speaks,
+			   		@Param("state") String state
+			   				
+			   		);
 	
 	
 }
--- src/main/java/com/crm/requestDTO/CompanyEmployeesUpdateAdminRequestDTO.java
+++ src/main/java/com/crm/requestDTO/CompanyEmployeesUpdateAdminRequestDTO.java
@@ -1,12 +1,13 @@
 package com.crm.requestDTO;
 
 
+
+import java.util.HashSet;
 import java.util.Set;
+
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import com.crm.domain.enums.Department;
@@ -19,18 +20,18 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class CompanyEmployeesRequestDTO {
+public class CompanyEmployeesUpdateAdminRequestDTO {
 
 	
 	
 	@Size(max = 50)
-	@NotNull
-	@NotBlank(message = "Please provide First name")
+	//@NotNull
+	//@NotBlank(message = "Please provide First name")
 	private String firstName;
 	
 	@Size(max = 50)
-	@NotNull
-	@NotBlank(message = "Please provide Last name")
+	//@NotNull
+	//@NotBlank(message = "Please provide Last name")
 	private String lastName;
 	
 	@Size(max = 50)
@@ -38,13 +39,8 @@ public class CompanyEmployeesRequestDTO {
 	//@NotNull(message = "Please provide email")
 	private String email;
 	
-	@Size(max = 30)
-	@NotNull(message = "Please provide password")
-	private String password;
-	
-	
 	@Size(max = 50)
-	@NotBlank(message = "Please provide job Title")
+	//@NotBlank(message = "Please provide job Title")
 	private String jobTitle;
 	
 	@Pattern(regexp = "^((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$", message = "Please enter valid phone number")
@@ -53,23 +49,23 @@ public class CompanyEmployeesRequestDTO {
 	private String phoneNumber;
 	
 	@Size(max = 255)
-	@NotNull(message = "Please provide address")
-	@NotBlank
+	//@NotNull(message = "Please provide address")
+	//@NotBlank
 	private String address;
 	
 	@Size(max = 20)
-	@NotNull(message = "Please provide city")
-	@NotBlank
+	//@NotNull(message = "Please provide city")
+	//@NotBlank
 	private String city;
 	
 	@Size(max = 50)
-	@NotNull(message = "Please provide country")
-	@NotBlank
+	//@NotNull(message = "Please provide country")
+	//@NotBlank
 	private String country;
 	
 	@Size(max = 20)
-	@NotNull(message = "Please provide state")
-	@NotBlank
+	//@NotNull(message = "Please provide state")
+	//@NotBlank
 	private String state;
 	
 	private Boolean hasWhatsapp;
@@ -78,14 +74,14 @@ public class CompanyEmployeesRequestDTO {
 	private String notes;
 	
 	@Size(max = 50)
-	@NotBlank
+	//@NotBlank
 	private String speaks;
-	
 	
 	private Boolean builtIn;
 
 	@Enumerated(EnumType.STRING)
 	private Department employeeDepartment;
+	
+	private Set<String> roles;
 
-	private Set<String> roles ;
 }
